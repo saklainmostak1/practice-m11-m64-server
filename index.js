efec6931dda73f4e7c32a4bad28d2bac1204c71b
saklainMostake <saklain15-12003@diu.edu.bd>
2022-11-01 03:20:10 +0600

    push

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -17,9 +18,27 @@ const users = [
     {id: 2, name: 'sabnur', email: 'sabnur@gmail.com' },
     {id: 3, name: 'sabila', email: 'sabila@gmail.com' },
 ];
+// uk0t8P6iUJJkseCh
+
+
+const uri = "mongodb+srv://dbuser1:uk0t8P6iUJJkseCh@cluster0.y3a2z0u.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get('/users', (req, res) =>{
-    res.send(users)
+    if(req.query.name){
+       const search = req.query.name
+       const filtered = users.filter(usr => usr.name.toLowerCase().indexOf(search) >= 0)
+       res.send(filtered) 
+    }
+    else{
+
+        res.send(users)
+    }
 } )
 app.post('/users', (req, res) =>{
     const user = req.body
