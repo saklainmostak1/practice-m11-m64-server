694d1a19a124bcb9fde22a0fb6295ccb7c604e5b
saklainMostake <saklain15-12003@diu.edu.bd>
2022-11-01 04:15:08 +0600

    push

--- index.js
+++ index.js
@@ -23,30 +23,47 @@ const users = [
 
 const uri = "mongodb+srv://dbuser1:uk0t8P6iUJJkseCh@cluster0.y3a2z0u.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
-app.get('/users', (req, res) =>{
-    if(req.query.name){
-       const search = req.query.name
-       const filtered = users.filter(usr => usr.name.toLowerCase().indexOf(search) >= 0)
-       res.send(filtered) 
+async function run() {
+    try{
+        const userCollection = client.db('simpleNode').collection('users')
+        app.get('/users', async(req,res) =>{
+            const cursor = userCollection.find({})
+            const users = await cursor.toArray()
+            res.send(users)
+        })
+        app.post('/users', async (req, res) =>{
+            const user = req.body
+            const result = await userCollection.insertOne(user)
+            user.id = result.insertedId
+            res.send(user)
+        })
     }
-    else{
+    finally{
 
-        res.send(users)
     }
-} )
-app.post('/users', (req, res) =>{
-    const user = req.body
-    user.id = users.length + 1
-    users.push(user)
-    console.log(user)
-    res.send(user)
-})
+
+}
+run().catch(err => console.log(err))
+
+// app.get('/users', (req, res) =>{
+//     if(req.query.name){
+//        const search = req.query.name
+//        const filtered = users.filter(usr => usr.name.toLowerCase().indexOf(search) >= 0)
+//        res.send(filtered) 
+//     }
+//     else{
+
+//         res.send(users)
+//     }
+// } )
+// app.post('/users', (req, res) =>{
+//     const user = req.body
+//     user.id = users.length + 1
+//     users.push(user)
+//     console.log(user)
+//     res.send(user)
+// })
 
 app.listen(port , (req, res) =>{
     console.log('api running on ', port)
